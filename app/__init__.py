70e951a92bc626951543351ed5b6b460f0fab46b
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2024-02-10 19:22:12 +0100

    add routes like django

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,13 @@ app.config['MONGODB_SETTINGS'] = {
 
 db = MongoEngine(app)
 
-from app.views import my_view
-from app.auth import auth_view
 
-app.register_blueprint(my_view)
-app.register_blueprint(auth_view)
+# from app.views import my_view
+# from app.auth import auth_view
+
+# app.register_blueprint(my_view)
+# app.register_blueprint(auth_view)
+
+
+from . import routes
 
--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, jsonify, request
 from .models import Movie
 
-my_view = Blueprint('my_view', __name__)
+# my_view = Blueprint('my_view', __name__)
 
 
-@my_view.route('/')
+# @my_view.route('/')
 def home():
     return  "Hello", 200
 
-@my_view.route('/movies/')
+# @my_view.route('/movies/')
 def get_movies():
     page = int(request.args.get('page',1))
     limit = int(request.args.get('limit',10))
@@ -16,13 +16,13 @@ def get_movies():
     return jsonify([movie.to_json() for movie in movies.items]), 200
 
     
-@my_view.route('/movies/<id>')
+# @my_view.route('/movies/<id>')
 def get_one_movie(id: str):
     movie = Movie.objects.get_or_404(id=id)
     return jsonify(movie.to_json()), 200
 
 
-@my_view.route('/movies/', methods=["POST"])
+# @my_view.route('/movies/', methods=["POST"])
 def add_movie():
     body = request.get_json()
     movie = Movie(**body).save()
